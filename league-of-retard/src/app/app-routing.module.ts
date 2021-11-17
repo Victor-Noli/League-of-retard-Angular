978f8a23b3d3ea407d17985efa702d5763dc4d46
Thibault Hale <thibault.hale@gmail.com>
2021-11-17 11:30:51 +0100

    - Update front Connexion/Inscription

--- league-of-retard/src/app/app-routing.module.ts
+++ league-of-retard/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ConnexionComponent} from "./pages/connexion/connexion.component";
 import {InscriptionComponent} from "./pages/inscription/inscription.component";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
 
 const routes: Routes = [
 
+  {path: '', component: HomePageComponent},
   {path: 'connexion', component: ConnexionComponent},
   {path: 'inscription', component: InscriptionComponent},
-
 
 ];
 
