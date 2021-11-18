28a0218b102855d0f9a5d7f119fbfcc70e7681ce
Thibault Hale <thibault.hale@gmail.com>
2021-11-18 21:06:01 +0100

    - Update front global

--- league-of-retard/src/app/app-routing.module.ts
+++ league-of-retard/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import {ConnexionComponent} from "./pages/connexion/connexion.component";
 import {InscriptionComponent} from "./pages/inscription/inscription.component";
 import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {UserProfilComponent} from "./pages/user-profil/user-profil.component";
+import {RechercheComponent} from "./pages/recherche/recherche.component";
 
 const routes: Routes = [
 
   {path: '', component: HomePageComponent},
   {path: 'connexion', component: ConnexionComponent},
   {path: 'inscription', component: InscriptionComponent},
+  {path: 'profil', component: UserProfilComponent},
+  {path: 'recherche', component: RechercheComponent},
 
 ];
 
--- league-of-retard/src/app/app.module.ts
+++ league-of-retard/src/app/app.module.ts
@@ -4,15 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { UserProfilComponent } from './pages/user-profil/user-profil.component';
+import { ConnexionComponent } from './pages/connexion/connexion.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { RechercheComponent } from './pages/recherche/recherche.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './pages/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    UserProfilComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    RechercheComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
